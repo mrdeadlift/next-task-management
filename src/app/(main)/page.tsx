cf09ff28a34e36edd3c77514ac5d63e589c0617b
mrdeadlift <mrdeadlift96@gmail.com>
2025-01-12 18:06:30 +0900

    task作成ページ実装

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function MainPage() {
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-20">
       <header className="flex justify-between items-center">
         <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
-        <Link href="/new-task" className="flex items-center gap-2 bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700">
+        <Link href="/new" className="flex items-center gap-2 bg-gray-800 text-white px-4 py-2 rounded-md hover:bg-gray-700">
           <MdAddTask className="size-5"/>
           <div>Add Task</div>
         </Link>
