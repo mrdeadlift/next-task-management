8c9e6ea292ec1706eb4cf315d9600b63c20fb7bf
mrdeadlift <mrdeadlift96@gmail.com>
2025-01-12 18:47:41 +0900

    feat:task編集ページ実装

---
+++ src/app/(main)/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import EditTaskForm from "@/components/EditTaskForm/EditTaskForm"
+
+const EditTaskPage = () => {
+  return (
+    <div>
+        <EditTaskForm />
+    </div>
+  )
+}
+
+export default EditTaskPage
