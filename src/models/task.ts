acec8e04e195b8cd4f9bf9fbbd3cf56484ba951f
mrdeadlift <mrdeadlift96@gmail.com>
2025-01-13 11:05:31 +0900

    feat:taskスキーマの実装

---
+++ src/models/task.ts
@@ -0,0 +1,23 @@
+import mongoose, {Document, Schema} from 'mongoose';
+
+interface Task extends Document {
+    title: string;
+    description: string;
+    dueDate: Date;
+    isCompleted: boolean;
+    }
+
+interface TaskModel extends Task, Document {
+    _id: string;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const taskSchema = new Schema<TaskModel>({
+    title: { type: String, required: true },
+    description: { type: String, required: false },
+    dueDate: { type: Date, required: false },
+    isCompleted: { type: Boolean, required: false, default: false },
+}, { timestamps: true });
+
+export const TaskModel = mongoose.models.Task || mongoose.model('Task', taskSchema);
